c6050b27aa9f0562f8eadd3eacc544940d2009cc
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 16:12:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,37 +5,22 @@ class Triangle
     @side_one=side_one
     @side_two=side_two
     @side_three=side_three
+    
   end
 
   def kind
-    self.not_valid?
-    if @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
-        "isosceles"
-    elsif @side_one==@side_two and @side_two==@side_three
-            "equilateral"
-
-    else
-      "scalene"
-    end
-  end
-
-  def not_valid?
-    if @side_one=0 || @side_two=0||  @side_three=0
-      begin
-        raise TriangleError
-      rescue TriangleError=>error
-        puts error.zero_message
-      end
-    elsif @side_one<0 || @side_two<0||  @side_three<0
-      begin
-        raise TriangleError
-      rescue TriangleError=>error
-        puts error.negative_message
+      
+      if @side_one==@side_two and @side_two==@side_three
+          "equilateral"
+      elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
+          "isosceles"
+      else
+        "scalene"
       end
     end
   end
-end
-
+  
+  
 class TriangleError < StandardError
   def zero_message
      "triangles with no size are illegal"
