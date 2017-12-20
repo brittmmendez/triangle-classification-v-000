5d8304ff5d47ee5a6b9b36b5301047d36814bc57
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 15:39:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,26 +7,31 @@ class Triangle
     @side_three=side_three
   end
 
-  def triangle_type
-    if @side_one==0 || @side_two==0||  @side_three==0
+  def valid?
+    if @side_one<=0 || @side_two<=0||  @side_three<=0
       begin
         raise TriangleError
       rescue TriangleError=>error
         puts error.message
       end
-    elsif @side_one==@side_two and @side_two==@side_three
-      "equilateral"
-    elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
-      "isosceles"
     else
-      "scalene"
+      self.triangle_type
     end
   end
 
+  def triangle_type
+    if @side_one==@side_two and @side_two==@side_three
+      "equilateral"
+    elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
+      "isosceles"
+    else 
+      "scalene"
+    end
+  end
 end
 
 class TriangleError < StandardError
  def message
-   "triangle can't hav a side equal to 0!"
+   "triangles violating triangle inequality are illegal"
  end
 end
