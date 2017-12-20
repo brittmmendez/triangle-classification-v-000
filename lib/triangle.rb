446536f43ee41c01c015da14399a094f558d0e3e
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 15:39:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,7 @@ class Triangle
     @side_one=side_one
     @side_two=side_two
     @side_three=side_three
+    valid?
   end
 
   def valid?
@@ -24,7 +25,7 @@ class Triangle
       "equilateral"
     elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
       "isosceles"
-    else 
+    else
       "scalene"
     end
   end
