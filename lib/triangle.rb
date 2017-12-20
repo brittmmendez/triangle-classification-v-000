85afbd7a8657837b108891bca5437c952e8389d3
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 15:34:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,19 +7,26 @@ class Triangle
     @side_three=side_three
   end
 
-  def valid?
-    if @side_one==0 || @side_two==0||  @side_three==0
-    end
-
-  end
-
   def triangle_type
-    if @side_one==@side_two and @side_two==@side_three
+    if @side_one==0 || @side_two==0||  @side_three==0
+      begin
+        raise TriangleError
+      rescue TriangleError=>error
+        puts error.message
+      end
+    elsif @side_one==@side_two and @side_two==@side_three
+      "equilateral"
+    elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
+      "isosceles"
+    else 
+      "scalene"
     end
   end
 
 end
 
 class TriangleError < StandardError
- # triangle error code
+ def message
+   "triangle can't hav a side equal to 0!"
+ end
 end
